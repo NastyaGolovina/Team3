3cc6dd815c1777b1ce008c8fb6cf94175ba2ea24
Anastasiia <n21koketka@gmail.com>
2024-10-03 18:33:23 +0300

    Add variables and methods to class SupplyCombinations

--- Team_3/src/main/java/UPT_PL/Team_3/SupplyCombinations.java
+++ Team_3/src/main/java/UPT_PL/Team_3/SupplyCombinations.java
@@ -1,9 +1,76 @@
 package UPT_PL.Team_3;
 
-public class SupplyCombinations {
+import java.util.ArrayList;
 
-	public SupplyCombinations() {
-		// TODO Auto-generated constructor stub
+public class SupplyCombinations {
+	//	Instance variable
+	private Country country;
+	private Product product;
+	private ArrayList<SupplyCombination> supplyCombinations;
+	
+	/**
+	 * Constructor
+	 * @param country
+	 * @param product
+	 * @param supplyCombinations
+	 */
+	public SupplyCombinations(Country country, Product product) {
+		super();
+		this.country = country;
+		this.product = product;
+		this.supplyCombinations = new ArrayList<SupplyCombination>();
 	}
 
+	/**
+	 * @return the country
+	 */
+	public Country getCountry() {
+		return country;
+	}
+
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	/**
+	 * @return the supplyCombinations
+	 */
+	public ArrayList<SupplyCombination> getSupplyCombinations() {
+		return supplyCombinations;
+	}
+
+	/**
+	 * @param supplyCombinations the supplyCombinations to set
+	 */
+	public void setSupplyCombinations(ArrayList<SupplyCombination> supplyCombinations) {
+		this.supplyCombinations = supplyCombinations;
+	}
+
+	@Override
+	public String toString() {
+		return "SupplyCombinations [country=" + country + ", product=" + product + ", supplyCombinations="
+				+ supplyCombinations + "]";
+	} 
+	
+	public void addSupplyCombination(SupplyCombination newSupplyCombination) {
+		supplyCombinations.add(newSupplyCombination);
+	}
+	
 }
