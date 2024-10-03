af015b8106a60cd4b088e14015bb330d2361bc15
Lenovo <Lenovo@DESKTOP-BGMSE5K>
2024-10-03 23:33:16 +0100

    Completed the country class with all methods and comments

--- Team_3/src/main/java/UPT_PL/Team_3/Country.java
+++ Team_3/src/main/java/UPT_PL/Team_3/Country.java
@@ -2,69 +2,153 @@ package UPT_PL.Team_3;
 
 import java.util.ArrayList;
 
+/**
+ * The Country class represents a country with an ID, name, population, products, and logistics sites.
+ * It provides methods to manage products and logistics sites within the country.
+ */
 public class Country {
-	private int id;
-	private String name;
-	private int population;
-	private ArrayList<ProductsByCountry> products;
-	private ArrayList<LogisticsSite> site;
+    private int id;
+    private String name;
+    private int population;
+    private ArrayList<ProductsByCountry> products;
+    private ArrayList<LogisticsSite> sites;
 
-	// constructors
-	public Country(int id, String name, int population) {
+    /**
+     * Constructor to initialize a Country object with the provided ID, name, and population.
+     * Initializes empty lists for products and logistics sites.
+     * 
+     * @param id The unique ID of the country.
+     * @param name The name of the country.
+     * @param population The population of the country.
+     */
+    public Country(int id, String name, int population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+        this.products = new ArrayList<>();  // Initialize products list
+        this.sites = new ArrayList<>();  // Initialize logistics sites list
+    }
+  
+    // Getters
+    /**
+     * Gets the ID of the country.
+     * 
+     * @return The country's ID.
+     */
+    public int getId() {
+        return id;
+    }
 
-		this.id = id;
-		this.name = name;
-		this.population = population;
-		this.products = new ArrayList<>();
-		this.site = new ArrayList<>();
-	}
+    /**
+     * Gets the name of the country.
+     * 
+     * @return The name of the country.
+     */
+    public String getName() {
+        return name;
+    }
 
-	// Getters
-	public int getId() {
-		return id;
-	}
+    /**
+     * Gets the population of the country.
+     * 
+     * @return The population size.
+     */
+    public int getPopulation() {
+        return population;
+    }
 
-	public String getName() {
-		return name;
-	}
+    /**
+     * Gets the list of products available in the country.
+     * 
+     * @return A list of ProductsByCountry objects.
+     */
+    public ArrayList<ProductsByCountry> getProducts() {
+        return products;
+    }
 
-	public int getPopulation() {
-		return population;
-	}
+    /**
+     * Gets the list of logistics sites in the country.
+     * 
+     * @return A list of LogisticsSite objects.
+     */
+    public ArrayList<LogisticsSite> getSites() {
+        return sites;
+    }
 
-	public ArrayList<ProductsByCountry> getProducts() {
-		return products;
-	}
+    // Setters
+    /**
+     * Sets the ID of the country.
+     * 
+     * @param id The new ID to set for the country.
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public ArrayList<LogisticsSite> getSite() {
-		return site;
-	}
+    /**
+     * Sets the name of the country.
+     * 
+     * @param name The new name to set for the country.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	// setters
-	public void setId(int id) {
-		this.id = id;
-	}
+    /**
+     * Sets the population of the country.
+     * 
+     * @param population The new population size to set for the country.
+     */
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Sets the list of products for the country.
+     * 
+     * @param products The new list of ProductsByCountry objects to set.
+     */
+    public void setProducts(ArrayList<ProductsByCountry> products) {
+        this.products = products;
+    }
 
-	public void setPopulation(int population) {
-		this.population = population;
-	}
+    /**
+     * Sets the list of logistics sites for the country.
+     * 
+     * @param sites The new list of LogisticsSite objects to set.
+     */
+    public void setSites(ArrayList<LogisticsSite> sites) {
+        this.sites = sites;
+    }
+    
+    // Methods
+    /**
+     * Searches for a product by its ID in the products list.
+     * 
+     * @param idProduct The ID of the product to search for.
+     * @return The index of the product in the products list, or -1 if not found.
+     */
+    public int searchProductByID(String idProduct) {
+    	
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i).getProduct();
+            
+            if (product.getidProduct().equalsIgnoreCase(idProduct)) {
+                return i;  // Return the index if a match is found
+            }
+        }
+        
+        // Return -1 if the product is not found
+        return -1;
+    }
 
-	public void setProducts(ArrayList<ProductsByCountry> products) {
-		this.products = products;
-	}
-
-	public void setSite(ArrayList<LogisticsSite> site) {
-		this.site = site;
-	}
-
-	@Override
-	public String toString() {
-		return "Country [id=" + id + ", name=" + name + ", population=" + population + ", products=" + products
-				+ ", site=" + site + "]";
-	}
+    /**
+     * Adds a new logistics site to the country.
+     */
+    public void addLogisticSite(String name, String country) {
+        ArrayList<Transport> emptyTransports = new ArrayList<>();
+        LogisticsSite logisticsite = new LogisticsSite(name, country, emptyTransports);
+        sites.add(logisticsite);
+    }
 
 }
