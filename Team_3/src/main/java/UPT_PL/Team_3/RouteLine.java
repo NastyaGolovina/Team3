12dac9b80e963a1d808c276cb576cf4f195306b9
Anastasiia <n21koketka@gmail.com>
2024-10-03 17:44:16 +0300

    Add RouteLine class variable and get() and set() methods

--- Team_3/src/main/java/UPT_PL/Team_3/RouteLine.java
+++ Team_3/src/main/java/UPT_PL/Team_3/RouteLine.java
@@ -3,10 +3,180 @@ package UPT_PL.Team_3;
 
 
 public class RouteLine {
-	//	Instance variable
+	//	Instance variable	
+	private Country countrySender;
+	private LogisticsSite logisticsSiteSender;
+	private Country countryReceiver;
+	private LogisticsSite logisticsSiteReceiver;
+	private Product product;
+	private Transport transport;
+	private double quantity;
+	private double amountProduct;
+	private double amountTransport;
+	private double totalAmount;
 	
-	public RouteLine() {
-		// TODO Auto-generated constructor stub
+	// Constructor
+	public RouteLine(Country countrySender, LogisticsSite logisticsSiteSender, Country countryReceiver,
+			LogisticsSite logisticsSiteReceiver, Product product, Transport transport, double quantity,
+			double amountProduct, double amountTransport, double totalAmount) {
+		super();
+		this.countrySender = countrySender;
+		this.logisticsSiteSender = logisticsSiteSender;
+		this.countryReceiver = countryReceiver;
+		this.logisticsSiteReceiver = logisticsSiteReceiver;
+		this.product = product;
+		this.transport = transport;
+		this.quantity = quantity;
+		this.amountProduct = amountProduct;
+		this.amountTransport = amountTransport;
+		this.totalAmount = totalAmount;
 	}
 
+	/**
+	 * @return the countrySender
+	 */
+	public Country getCountrySender() {
+		return countrySender;
+	}
+
+	/**
+	 * @param countrySender the countrySender to set
+	 */
+	public void setCountrySender(Country countrySender) {
+		this.countrySender = countrySender;
+	}
+
+	/**
+	 * @return the logisticsSiteSender
+	 */
+	public LogisticsSite getLogisticsSiteSender() {
+		return logisticsSiteSender;
+	}
+
+	/**
+	 * @param logisticsSiteSender the logisticsSiteSender to set
+	 */
+	public void setLogisticsSiteSender(LogisticsSite logisticsSiteSender) {
+		this.logisticsSiteSender = logisticsSiteSender;
+	}
+
+	/**
+	 * @return the countryReceiver
+	 */
+	public Country getCountryReceiver() {
+		return countryReceiver;
+	}
+
+	/**
+	 * @param countryReceiver the countryReceiver to set
+	 */
+	public void setCountryReceiver(Country countryReceiver) {
+		this.countryReceiver = countryReceiver;
+	}
+
+	/**
+	 * @return the logisticsSiteReceiver
+	 */
+	public LogisticsSite getLogisticsSiteReceiver() {
+		return logisticsSiteReceiver;
+	}
+
+	/**
+	 * @param logisticsSiteReceiver the logisticsSiteReceiver to set
+	 */
+	public void setLogisticsSiteReceiver(LogisticsSite logisticsSiteReceiver) {
+		this.logisticsSiteReceiver = logisticsSiteReceiver;
+	}
+
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	/**
+	 * @return the transport
+	 */
+	public Transport getTransport() {
+		return transport;
+	}
+
+	/**
+	 * @param transport the transport to set
+	 */
+	public void setTransport(Transport transport) {
+		this.transport = transport;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public double getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the amountProduct
+	 */
+	public double getAmountProduct() {
+		return amountProduct;
+	}
+
+	/**
+	 * @param amountProduct the amountProduct to set
+	 */
+	public void setAmountProduct(double amountProduct) {
+		this.amountProduct = amountProduct;
+	}
+
+	/**
+	 * @return the amountTransport
+	 */
+	public double getAmountTransport() {
+		return amountTransport;
+	}
+
+	/**
+	 * @param amountTransport the amountTransport to set
+	 */
+	public void setAmountTransport(double amountTransport) {
+		this.amountTransport = amountTransport;
+	}
+
+	/**
+	 * @return the totalAmount
+	 */
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	/**
+	 * @param totalAmount the totalAmount to set
+	 */
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	@Override
+	public String toString() {
+		return "RouteLine [countrySender=" + countrySender + ", logisticsSiteSender=" + logisticsSiteSender
+				+ ", countryReceiver=" + countryReceiver + ", logisticsSiteReceiver=" + logisticsSiteReceiver
+				+ ", product=" + product + ", transport=" + transport + ", quantity=" + quantity + ", amountProduct="
+				+ amountProduct + ", amountTransport=" + amountTransport + ", totalAmount=" + totalAmount + "]";
+	}
 }
