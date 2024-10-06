89089848e9a7aff1adf636f66590e7f90923d053
dashscode <m4daria@gmail.com>
2024-10-06 23:34:43 +0200

    Add LogisticsSupplyChains class

--- Team_3/src/main/java/UPT_PL/Team_3/LogisticsSupplyChains.java
+++ Team_3/src/main/java/UPT_PL/Team_3/LogisticsSupplyChains.java
@@ -3,5 +3,20 @@ package UPT_PL.Team_3;
 import java.util.ArrayList;
 
 public class LogisticsSupplyChains {
+    private ArrayList<LogisticsSupplyChain> supplyChains;
 
+    public LogisticsSupplyChains() {
+        this.supplyChains = new ArrayList<>();
+    }
+
+    public void addSupplyChain(LogisticsSupplyChain logisticsSupplyChain) {
+        if (logisticsSupplyChain == null) {
+            throw new IllegalArgumentException("Supply chain cannot be null");
+        }
+        supplyChains.add(logisticsSupplyChain);
+    }
+
+    public ArrayList<LogisticsSupplyChain> getSupplyChains() {
+        return supplyChains;
+    }
 }
