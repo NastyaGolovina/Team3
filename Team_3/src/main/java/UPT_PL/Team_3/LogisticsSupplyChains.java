f86fd89cc58831f5d63f1f7cb764b62ddc5506c9
dashscode <m4daria@gmail.com>
2024-10-06 23:42:12 +0200

    Add JavaDoc style for LogisticsSupplyChains.java

--- Team_3/src/main/java/UPT_PL/Team_3/LogisticsSupplyChains.java
+++ Team_3/src/main/java/UPT_PL/Team_3/LogisticsSupplyChains.java
@@ -2,13 +2,25 @@ package UPT_PL.Team_3;
 
 import java.util.ArrayList;
 
+/**
+ * The LogisticsSupplyChains class manages a collection of LogisticsSupplyChain objects.
+ */
 public class LogisticsSupplyChains {
     private ArrayList<LogisticsSupplyChain> supplyChains;
 
+    /**
+     * Constructor that initializes an empty list of supply chains.
+     */
     public LogisticsSupplyChains() {
         this.supplyChains = new ArrayList<>();
     }
 
+    /**
+     * Adds a LogisticsSupplyChain to the list.
+     * Throws an IllegalArgumentException if the provided object is null.
+     *
+     * @param logisticsSupplyChain the supply chain to be added, cannot be null
+     */
     public void addSupplyChain(LogisticsSupplyChain logisticsSupplyChain) {
         if (logisticsSupplyChain == null) {
             throw new IllegalArgumentException("Supply chain cannot be null");
@@ -16,6 +28,11 @@ public class LogisticsSupplyChains {
         supplyChains.add(logisticsSupplyChain);
     }
 
+    /**
+     * Returns the list of LogisticsSupplyChain objects.
+     *
+     * @return the list of supply chains
+     */
     public ArrayList<LogisticsSupplyChain> getSupplyChains() {
         return supplyChains;
     }
