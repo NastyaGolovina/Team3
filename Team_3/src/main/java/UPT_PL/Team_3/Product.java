074257ac47011ee3a40bb38862b37c22f346b25f
Lyly Nguyen <lyyngn1723@gmail.com>
2024-10-08 17:25:11 +0100

    comment and changed name of variable in Product class

--- Team_3/src/main/java/UPT_PL/Team_3/Product.java
+++ Team_3/src/main/java/UPT_PL/Team_3/Product.java
@@ -18,30 +18,63 @@ public Product(String productId, String name, Integer experiationIndays, double 
 	this.experiationIndays = experiationIndays;
 	this.recommenedRate = recommenedRate;
 }
-public String getProductID() {
+
+/**
+ * @return the productId
+ */
+public String getProductId() {
 	return productId;
 }
-public void setProductID(String productId) {
+
+/**
+ * @param productId the productId to set
+ */
+public void setProductId(String productId) {
 	this.productId = productId;
 }
+
+/**
+ * @return the name
+ */
 public String getName() {
 	return name;
 }
+
+/**
+ * @param name the name to set
+ */
 public void setName(String name) {
 	this.name = name;
 }
+
+/**
+ * @return the experiationIndays
+ */
 public Integer getExperiationIndays() {
 	return experiationIndays;
 }
+
+/**
+ * @param experiationIndays the experiationIndays to set
+ */
 public void setExperiationIndays(Integer experiationIndays) {
 	this.experiationIndays = experiationIndays;
 }
-public double getrecommenedRate() {
+
+/**
+ * @return the recommenedRate
+ */
+public double getRecommenedRate() {
 	return recommenedRate;
 }
-public void setrecommenedRate(double recommenedRate) {
+
+/**
+ * @param recommenedRate the recommenedRate to set
+ */
+public void setRecommenedRate(double recommenedRate) {
 	this.recommenedRate = recommenedRate;
 }
+
 @Override
 public String toString() {
 	return "Product [ProductID=" + productId + ", Name=" + name + ", ExperiationIndays=" + experiationIndays
