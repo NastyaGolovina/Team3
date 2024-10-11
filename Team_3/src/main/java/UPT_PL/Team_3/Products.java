438cae1d0b8d1224a95b014ef183316a2e5bd6bb
Lyly Nguyen <lyyngn1723@gmail.com>
2024-10-11 14:14:23 +0100

    Generate comment for Products class

--- Team_3/src/main/java/UPT_PL/Team_3/Products.java
+++ Team_3/src/main/java/UPT_PL/Team_3/Products.java
@@ -1,59 +1,69 @@
 package UPT_PL.Team_3;
 
 import java.util.ArrayList;
+
+/**
+ * The Products class represents a collection/list of Product objects. It
+ * provides methods to add new product to the list , to search and manage the collection.
+ */
+ 
 public class Products {
-private ArrayList<Product> ProductList;
+// variable
+private ArrayList<Product> ProductList; // list name ProductList to store all of the products from Product class
 
-
-// constructor
+/** Constructor to initialize the Products object with an empty list of products
+ * 
+ */
 	public Products () {
 		ProductList = new ArrayList<Product>();
 		}
-// get
 	
-	/**
-	 * @return the productList
-	 */
+/** Get the list of the product
+* 
+* @return the ProductList
+*/
 	public ArrayList<Product> getProductList() {
 		return ProductList;
 	}
 
-	// set
-	/**
-	 * @param productList the productList to set
-	 */
+	
+/** Set the list of the product
+ * 
+ * @param ProductList the new ProductList to set
+ */
 	public void setProductList(ArrayList<Product> ProductList) {
 		this.ProductList = ProductList;
 		
 	}
 	
 	
-	// search for product existence 
-	// this method search through the list of products to find a specific product by the unique productId
-	// and return the index/position of that product in the array list, if it is found
-	// if not been found in the list, return -1
-	
-	public int searchProduct(String productId) {
-		int i = 0; // initialize the loop, set the i = 0 which is the starting point of the array list 
-		while (i< ProductList.size() && ! ProductList.get(i).getProductID().equalsIgnoreCase(productId)) {
+	/** Method to search for a product in the list by its unique productID
+	 * This method searches through the list to find a product by its productID
+	 * 
+	 * @param productID  The unique ID of the product
+	 * @return the index/position of the product if it is found
+	 * @return -1 if it is not found 
+	 */
+	public int searchProduct(String productID) {
+		//Initialize the counter to track the index of product in the list
+		 // set the i = 0 which is the starting point of the array list
+		int i = 0;
+		while (i< ProductList.size() && ! ProductList.get(i).getProductID().equalsIgnoreCase(productID)) {
 			i++; // increment i to move to the next element of the list to keep searching 
 			}
 		if (i != ProductList.size()) {
-			return i; 
+			return i; // if the product is found
 		}
 		return -1; // return -1 if it is not found
 	}
 	
-	/**
-	 *  @param idPoroduct
+	/** Method to add new product to the ProductList
+	 * 
+	 * @param idPoroduct
 	 * @param name
 	 * @param experiationIndays
 	 * @param healthyRate
-	 * addProduct
 	 */
-
-	
-	// more specific add product method 
 	
 	public void addProduct() {
 		String productId = ProjectHelper.inputStr("Input product ID : ");
@@ -64,15 +74,14 @@ private ArrayList<Product> ProductList;
 		 }
 		 
 		int productPos = searchProduct(productId); // Product position in the array list
-		// searchProduct by the product Id
+		// searchProduct by the productId
 		if (productPos != -1) { //in case the product is not found in the list by checking its ID then print existed
             //then move to the add product starting by name
 		                    
 			System.out.println("Product already existed");
 		}
 		
-		
-			else { // starting to add new product follow by those variables
+		else { // starting to add new product follow by those variables
 			 String name = ProjectHelper.inputStr("Input product name: "); 
 			   if (name.isEmpty()) {
 				   System.out.println("The product name can not be empty, please insert the name! ");
@@ -97,8 +106,9 @@ private ArrayList<Product> ProductList;
 	
 	}
 	
-	
-	// Method to display the ProductList
+	/** Method to display the ProductList
+	 * 
+	 */
     public void displayProducts() {
         if (ProductList.isEmpty()) {
             System.out.println("The product list is empty.");
