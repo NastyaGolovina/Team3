48d1c9e0d4cfc889c5bddf8bed788212ab32c515
Anastasiia <n21koketka@gmail.com>
2024-10-14 00:29:27 +0300

    Database initialization

--- Team_3/src/main/java/UPT_PL/Team_3/Product.java
+++ Team_3/src/main/java/UPT_PL/Team_3/Product.java
@@ -4,6 +4,14 @@ package UPT_PL.Team_3;
  * Each product contains information such as 
  * unique productID,name, expirationInDays and recommenedRate
  */
+
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+
 public class Product {
 private String productID;
 private String name;
