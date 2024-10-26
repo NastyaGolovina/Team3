7aa76252dc637cb93e9d076701d7626988741a41
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-10-26 22:58:25 +0100

    basic work with jakarta)

--- Team_3/src/main/java/UPT_PL/Team_3/LogisticsSupplyChain.java
+++ Team_3/src/main/java/UPT_PL/Team_3/LogisticsSupplyChain.java
@@ -1,16 +1,39 @@
 package UPT_PL.Team_3;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 /**
  * The LogisticsSupplyChain class represents a supply chain between two logistics sites.
  */
+@Entity
+@Table(name = "Logistics_Supply_Chain")
 public class LogisticsSupplyChain {
+	@Id
+    @Column(name = "chain_id", length = 40, nullable = false)
     private String chainId;
-    private LogisticsSite sender;
-    private LogisticsSite receiver;
-    private Transport transport;
-    private double costPerTon;
-    private double durationInDays;
 
+    @ManyToOne
+    @JoinColumn(name = "sender_site_id", nullable = false)
+    private LogisticsSite sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver", nullable = false)
+    private LogisticsSite receiver;
+
+    @ManyToOne
+    @JoinColumn(name = "Transport_Id", nullable = false)
+    private Transport transport;
+
+    @Column(name = "cost_per_ton", nullable = false)
+    private double costPerTon;
+
+    @Column(name = "duration_in_days", nullable = false)
+    private double durationInDays;
     /**
      * Constructor to initialize a LogisticsSupplyChain with all necessary details.
      *
